46e75bf669ea83f9907cfefac4c7cec1465ee121
AmanB-2003 <90694309+AmanB-2003@users.noreply.github.com>
2023-11-18 03:14:31 +0530

    Add files via upload

---
+++ Eth_Login/solidity/truffle-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "localhost",
+      port: 7545,
+      network_id: "*", // Match any network id
+      from: '0x851C5c21E82bC148Df7EFC28F074441689b73807'
+    }
+  }
+};
